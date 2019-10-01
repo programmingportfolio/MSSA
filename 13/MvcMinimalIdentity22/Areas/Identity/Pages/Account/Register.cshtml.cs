0fdd62d12a7b7ca4e28bec304da055186bcc1ee6
LUke <porterl2268@student.faytechcc.edu>
2019-10-01 08:03:48 -0400

    everything setup except for EmailSender

--- 13/MvcMinimalIdentity22/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ 13/MvcMinimalIdentity22/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,15 +19,18 @@ namespace MvcMinimalIdentity22.Areas.Identity.Pages.Account
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            ILogger<RegisterModel> logger)
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _emailSender = emailSender;
         }
 
         [BindProperty]
